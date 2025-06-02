7fa874db1e6e85a80b932348b44837e23398c607
cheers8oys <cheers8oys@gmail.com>
2025-06-02 15:16:45 +0900

    MVC와 템플릿 엔진

--- src/main/java/com/hello_spring/spring/controller/HelloController.java
+++ src/main/java/com/hello_spring/spring/controller/HelloController.java
@@ -4,6 +4,7 @@ package com.hello_spring.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hello_mvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
